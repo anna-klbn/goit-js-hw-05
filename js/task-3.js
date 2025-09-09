0129a2ffff6c37d5007e2324ed49f26d4704fef3
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2025-09-09 22:01:41 +0300

    task-2,3,4

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,18 @@
-
-// Напиши клас `Storage`, який буде створювати об'єкти для управління складом
-// товарів. При виклику буде отримувати один аргумент - початковий масив товарів, і
-// записувати його у властивість `items`.
-
-// Додай методи класу:
-
-// - `getItems()` - повертає масив поточних товарів
-// - `addItem(item)` - отримує новий товар і додає його до поточних
-// - `removeItem(item)` - отримує товар і, якщо він є, видаляє його з поточних
-
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems(){
+  return this.items
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+     items.splice(this.items.indexOf(item), 1);
+  
+  }
+}
 
 const storage = new Storage([
   "Нанітоіди",
@@ -18,8 +22,6 @@ const storage = new Storage([
 ]);
 
 const items = storage.getItems();
-console.log(items)
-
 console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
 
 storage.addItem("Дроїд");
